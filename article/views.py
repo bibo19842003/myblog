166c6707e87f5d1c0f35c36f1a784be9dd54b3b1
bibo <bibo19842003@163.com>
2021-10-13 09:00:51 +0800

    Add Pagination for article list.

--- article/views.py
+++ article/views.py
@@ -8,6 +8,7 @@ from .forms import ArticlePostForm
 # 引入User模型
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 @login_required(login_url='/userprofile/login/')
@@ -42,11 +43,16 @@ def article_create(request):
 
 
 def article_list(request):
-    # 取出所有博客文章
-    articles = ArticlePost.objects.all()
-    # 需要传递给模板（templates）的对象
+    article_list = ArticlePost.objects.all()
+
+    # 每页显示 3 篇文章
+    paginator = Paginator(article_list, 3)
+    # 获取 url 中的页码
+    page = request.GET.get('page')
+    # 将导航对象相应的页码内容返回给 articles
+    articles = paginator.get_page(page)
+
     context = { 'articles': articles }
-    # render函数：载入模板，并返回context对象
     return render(request, 'article/list.html', context)
 
 
