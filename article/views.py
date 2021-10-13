3cb5bfbfdf7169a9adbc48310fddc55084aba174
bibo <bibo19842003@163.com>
2021-10-13 09:42:58 +0800

    Add order for list article page.
    two order: 1 hot, 2 date

--- article/views.py
+++ article/views.py
@@ -43,7 +43,13 @@ def article_create(request):
 
 
 def article_list(request):
-    article_list = ArticlePost.objects.all()
+    # 根据GET请求中查询条件 返回不同排序的对象数组
+    if request.GET.get('order') == 'total_views':
+        article_list = ArticlePost.objects.all().order_by('-total_views')
+        order = 'total_views'
+    else:
+        article_list = ArticlePost.objects.all()
+        order = 'normal'
 
     # 每页显示 3 篇文章
     paginator = Paginator(article_list, 3)
@@ -52,7 +58,8 @@ def article_list(request):
     # 将导航对象相应的页码内容返回给 articles
     articles = paginator.get_page(page)
 
-    context = { 'articles': articles }
+    context = { 'articles': articles, 'order': order }
+
     return render(request, 'article/list.html', context)
 
 
